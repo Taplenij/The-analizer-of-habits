9d0a8e0660aad8763cdc83b4963c51a85dc12049
Taplenij <bodilovskijegor87@gmail.com>
2025-08-13 22:15:07 +0300

    new gui

--- get_open_window.py
+++ get_open_window.py
@@ -1,7 +1,7 @@
 import pyautogui as pg
 
 
-def get_win_title():
+async def get_win_title():
     active_window = pg.getActiveWindow()
     if active_window:
         return active_window.title
---
+++ gui.py
@@ -0,0 +1,8 @@
+from tkinter import *
+from tkinter import ttk
+
+root = Tk()
+root.geometry('900x700')
+root.config(bg='darkgray')
+
+root.mainloop()

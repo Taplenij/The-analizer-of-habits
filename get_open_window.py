a4cf657ab7e0e1e1119dc8d714a52ab2e0ecf130
Taplenij <bodilovskijegor87@gmail.com>
2025-08-13 20:52:13 +0300

    function get_win_title

--- get_open_window.py
+++ get_open_window.py
@@ -4,6 +4,6 @@ import pyautogui as pg
 def get_win_title():
     active_window = pg.getActiveWindow()
     if active_window:
-        return active_window.title()
+        return active_window.title
     else:
         return None

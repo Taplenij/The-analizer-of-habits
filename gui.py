1f51a6b4a71b59efc69f979281c5abae5b9391bb
Taplenij <bodilovskijegor87@gmail.com>
2025-08-17 16:15:57 +0300

    Finished create gui

--- gui.py
+++ gui.py
@@ -13,23 +13,41 @@ def setting_win():
     win.title('Settings')
     win.geometry('800x500')
     win.config(bg='darkgray')
+    win.resizable(False, False)
 
     frequency = ttk.Label(win, text='Set the analysis frequency:\n'
                                     'THE RESULTS ACCURACY WILL DIFFER!',
-                          background='darkgray', font=('Arial', 12))
+                          background='darkgray', font=('Arial', 14))
     frequency.place(x=1, y=1)
     # Create a combobox
     times = [f'{i} min' for i in range(5, 31, 5)]
     times_var = StringVar(value=times[0])
     set_freq = ttk.Combobox(win, textvariable=times_var, values=times, font=('Arial', 10))
-    set_freq.place(x=1, y=40)
+    set_freq.place(x=1, y=50)
     # I was too lazy to draw a line, so I just made a black canvas
-    canvas1 = Canvas(win, bg='black', highlightthickness=0, width=1, height=1080)
-    canvas1.pack(fill=Y)
-
+    canvas1 = Canvas(win, bg='black', highlightthickness=0, width=1, height=500)
+    canvas1.place(x=400, y=0)
+    # Format type label
     print_type = ttk.Label(win, text='Choose the format type output:',
-                           background='darkgray', font=('Arial', 12))
-    print_type.place(x=400, y=1)
+                           background='darkgray', font=('Arial', 14))
+    print_type.place(x=410, y=0)
+
+    enabled = StringVar()
+    # Create checkbutton(table, plot, table and plot)
+    table_check = ttk.Checkbutton(win, text='Table', variable=enabled, width=7, onvalue='table')
+    table_check.place(x=410, y=30)
+    plot_check = ttk.Checkbutton(win, text='Plot', variable=enabled,  width=7, onvalue='plot')
+    plot_check.place(x=410, y=60)
+    table_plot_check = ttk.Checkbutton(win, text='Table and Plot', onvalue='table_plot')
+    table_plot_check.place(x=410, y=90)
+    # Notifications checkbutton
+    notifications = StringVar(value='yes')
+
+    not_lab = ttk.Label(win, text='Receive notifications',
+                        background='darkgray', font=('Arial', 14))
+    not_lab.place(x=1, y=300)
+    rec_not = ttk.Checkbutton(win, variable=notifications, width=0, onvalue='yes', offvalue='no')
+    rec_not.place(x=190, y=305)
 
 
 # Create the main window

2559a6a0c7a64d0f409263ee6a9f36f9d042ba2a
Taplenij <bodilovskijegor87@gmail.com>
2025-08-18 16:51:25 +0300

    Now function get_win_title asynchronous

--- data/user_activity.py
+++ data/user_activity.py
@@ -1,10 +1,24 @@
 import pyautogui as pg
+import asyncio
 
 
 # Catch the open window
 async def get_win_title():
-    active_window = pg.getActiveWindow()
-    if active_window:
-        return active_window.title
-    else:
-        return None
+    while True:
+        active_window = pg.getActiveWindow().title
+        if active_window:
+            print(active_window)
+        else:
+            print('None')
+        await asyncio.sleep(1)
+
+
+async def main():
+    task = asyncio.create_task(get_win_title())
+    while True:
+        await asyncio.sleep(1)
+
+try:
+    asyncio.run(main())
+except KeyboardInterrupt:
+    print('leave')

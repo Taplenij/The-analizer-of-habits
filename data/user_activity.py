5c0735f04fa24e95150fa0a7d58a8898f8b7267b
Taplenij <bodilovskijegor87@gmail.com>
2025-08-18 21:16:42 +0300

    Function time_pos that tracks the mouse cursor position

--- data/user_activity.py
+++ data/user_activity.py
@@ -1,5 +1,6 @@
 import pyautogui as pg
 import asyncio
+from datetime import datetime
 
 
 # Catch the open window
@@ -13,8 +14,20 @@ async def get_win_title():
         await asyncio.sleep(1)
 
 
+async def time_pos():
+    while True:
+        pos1 = pg.position()
+        await asyncio.sleep(120)
+        pos2 = pg.position()
+        return True if pos1 == pos2 else False
+
+
+# async def timer():
+
+
 async def main():
     task = asyncio.create_task(get_win_title())
+    task2 = asyncio.create_task(time_pos())
     while True:
         await asyncio.sleep(1)
 

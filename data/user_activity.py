e42a66b49d64d76731fcc8ae82208187f410cde0
Taplenij <bodilovskijegor87@gmail.com>
2025-08-21 17:11:33 +0300

    Created manage_stopwatch function that pinpoint how long window is use

--- data/user_activity.py
+++ data/user_activity.py
@@ -1,35 +1,65 @@
 import pyautogui as pg
 import asyncio
-from datetime import datetime
+from stwat import StopWatch
+
+
+sw = StopWatch()
 
 
 # Catch the open window
 async def get_win_title():
     while True:
-        active_window = pg.getActiveWindow().title
-        if active_window:
-            print(active_window)
+        active_window = pg.getActiveWindow()
+        return active_window.title if active_window else False
+
+
+async def manage_stopwatch(stop_watch: StopWatch, cur_st, next_st):
+    stop_watch.FLAG = True  # Default FLAG value
+    time_el = stop_watch.ELAPSED_TIME.format(0, 0, 0)  # Initialize from what date stopwatch will begin
+    while stop_watch.FLAG and stop_watch.RUNNING:
+        if cur_st and cur_st == next_st:  # If window is open and is use...
+            print(time_el)  # ...start stopwatch
+            await asyncio.sleep(1)
+            time_el = await stop_watch.increment()
+            next_st = await get_win_title()  # Check if new window is open. If it is...
         else:
-            print('None')
-        await asyncio.sleep(1)
+            stop_watch.FLAG = False  # ...stop stopwatch
+    await stop_watch.stop()
+    return
 
 
+# Catch cursor position
 async def time_pos():
     while True:
         pos1 = pg.position()
         await asyncio.sleep(120)
         pos2 = pg.position()
-        return True if pos1 == pos2 else False
+        if pos1 != pos2:
+            return True
 
 
-# async def timer():
+#  Start stopwatch function
+async def monitor_win():
+    while True:
+        window = pg.getActiveWindow()  # Catch active window
+        current_state = window.title if window else False
+        # await asyncio.sleep(1)
+        print(current_state)
+        await manage_stopwatch(sw, current_state, await get_win_title())  # Start stopwatch
 
 
 async def main():
-    task = asyncio.create_task(get_win_title())
-    task2 = asyncio.create_task(time_pos())
-    while True:
-        await asyncio.sleep(1)
+    win_mon = asyncio.create_task(monitor_win())
+    pos_mon = asyncio.create_task(time_pos())
+    try:
+        while True:
+            await asyncio.sleep(1)
+    except KeyboardInterrupt:
+        print('interrupted')
+    except Exception as e:
+        print(f'EXCEPTION {e}')
+    finally:
+        win_mon.cancel()
 
 try:
     asyncio.run(main())

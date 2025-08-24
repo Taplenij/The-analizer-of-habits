8b981a6f01ccdd134d2d71edc68419027add9a85
Taplenij <bodilovskijegor87@gmail.com>
2025-08-24 19:32:30 +0300

    Added remove_all function

--- data/requests.py
+++ data/requests.py
@@ -35,9 +35,17 @@ class DBC:
             except pg.exceptions.UniqueViolationError:
                 print('This id already exists in database')
 
-# FIXME
     async def rec_app(self, app):
         if not self.pool:
             raise RuntimeError('Pool is not initialized')
         async with self.pool.acquire as con:
-            pass
+            try:
+                await con.execute('INSERT INTO app_list(app_name) VALUE $1', app)
+            except pg.exceptions.UniqueViolationError:
+                print('This app is already in database')
+
+    async def remove_all(self):
+        if not self.pool:
+            raise RuntimeError('Pool is not initialized')
+        async with self.pool.acquire as con:
+            await con.execute('DELETE FROM user_activity')

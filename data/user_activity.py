2e9e6181b27dd439871643b57391d8067e024b1f
Taplenij <bodilovskijegor87@gmail.com>
2025-08-26 16:18:28 +0300

    Added remove_all function

--- data/user_activity.py
+++ data/user_activity.py
@@ -8,58 +8,65 @@ sw = StopWatch()
 
 # Catch the open window
 async def get_win_title():
-    while True:
-        active_window = pg.getActiveWindow()
-        return active_window.title.split(' - ')[-1] if active_window else False
+    active_window = pg.getActiveWindow()
+    return active_window.title.split(' - ')[-1] if active_window else False
 
 
 async def manage_stopwatch(stop_watch: StopWatch, cur_st, next_st):
     stop_watch.FLAG = True  # Default FLAG value
     time_el = stop_watch.ELAPSED_TIME.format(0, 0, 0)  # Initialize from what date stopwatch will begin
-    while stop_watch.FLAG and stop_watch.RUNNING:
-        if cur_st and cur_st == next_st:  # If window is open and is use...
-            print(f'\r{time_el}', end=' ')  # ...start stopwatch
+    mouse_state = True
+
+    async def mouse_pos():
+        nonlocal mouse_state
+        while True:
+            pos1 = pg.position()
+            await asyncio.sleep(5)
+            pos2 = pg.position()
+            mouse_state = pos1 != pos2
+
+    mouse_task = asyncio.create_task(mouse_pos())  # Start mouse_pos function
+    try:                                            # on background
+        while stop_watch.FLAG and stop_watch.RUNNING:
+            if cur_st and cur_st == next_st and mouse_state:  # If window is open
+                # and is use...
+                print(f'\r{time_el}', end=' ')  # ...start stopwatch
+                time_el = await stop_watch.increment()
+                next_st = await get_win_title()  # Check if new window is open.
+            else:                                  # If it is...
+                stop_watch.FLAG = False  # ...stop stopwatch
+                await sw.stop()
             await asyncio.sleep(1)
-            time_el = await stop_watch.increment()
-            next_st = await get_win_title()  # Check if new window is open. If it is...
-        else:
-            stop_watch.FLAG = False  # ...stop stopwatch
-    await stop_watch.stop()
-    return
+    finally:
+        mouse_task.cancel()
 
 
 # Catch cursor position
-async def time_pos():
-    while True:
-        pos1 = pg.position()
-        await asyncio.sleep(120)
-        pos2 = pg.position()
-        if pos1 != pos2:
-            return True
+async def mouse_pos():
+    pos1 = pg.position()
+    await asyncio.sleep(5)
+    pos2 = pg.position()
+    return pos1 != pos2
 
 
-#  Start stopwatch function
+# Start stopwatch function
 async def monitor_win():
     while True:
         current_state = await get_win_title()  # Catch active window
-        await asyncio.sleep(1)
         print(current_state)
-        await manage_stopwatch(sw, current_state, await get_win_title())  # Start stopwatch
+        await manage_stopwatch(sw, current_state, current_state)  # Start stopwatch
 
 
 async def main():
     win_mon = asyncio.create_task(monitor_win())
-    pos_mon = asyncio.create_task(time_pos())
     try:
         while True:
+            await win_mon
             await asyncio.sleep(1)
     except KeyboardInterrupt:
         print('interrupted')
     except Exception as e:
         print(f'EXCEPTION {e}')
-    finally:
-        win_mon.cancel()
-
 try:
     asyncio.run(main())
 except KeyboardInterrupt:

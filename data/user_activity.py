a9d58ea447f3605bb00512e1270a6256d21b49fe
Taplenij <bodilovskijegor87@gmail.com>
2025-08-28 18:33:17 +0300

    Refactoring

--- data/user_activity.py
+++ data/user_activity.py
@@ -1,62 +1,78 @@
 import pyautogui as pg
 import asyncio
 from stwat import StopWatch
-import queue
 
 
-sw = StopWatch()
+class UserActivity:
+    def __init__(self):
+        self._stopwatch = StopWatch()
+        self._stopwatch.FLAG = True
+        self._stopwatch.RUNNING = True
+        self._INFO = dict()
+        self._next_state = None
+        self._current_state = None
+        self._elapsed_time = None
+        self._mouse_state = True
 
+    @staticmethod
+    async def _get_title():
+        active_window = pg.getActiveWindow()
+        return active_window.title.split(' - ')[-1] if active_window else False
 
-# Catch the open window
-async def get_win_title():
-    active_window = pg.getActiveWindow()
-    return active_window.title.split(' - ')[-1] if active_window else False
-
-
-async def manage_stopwatch(stop_watch: StopWatch, cur_st, next_st):
-    stop_watch.FLAG = True  # Default FLAG value
-    time_el = stop_watch.ELAPSED_TIME.format(0, 0, 1)  # Initialize from what date stopwatch will begin
-    mouse_state = True
-
-    async def mouse_pos():
-        nonlocal mouse_state
+    async def _mouse_pos(self):
         while True:
             pos1 = pg.position()
-            await asyncio.sleep(0.0001)
+            await asyncio.sleep(5)
             pos2 = pg.position()
-            mouse_state = pos1 != pos2
+            self._mouse_state = pos1 != pos2
 
-    mouse_task = asyncio.create_task(mouse_pos())  # Start mouse_pos function
-    try:                                            # on background
-        while stop_watch.FLAG and stop_watch.RUNNING:
-            if cur_st and cur_st == next_st and mouse_state:  # If window is open
-                # and is use...
-                print(f'\r{time_el}', end=' ')  # ...start stopwatch
-                time_el = await stop_watch.increment()
-                next_st = await get_win_title()  # Check if new window is open.
-            else:                                  # If it is...
-                stop_watch.FLAG = False  # ...stop stopwatch
-                await sw.stop()
-            await asyncio.sleep(1)
-    finally:
-        mouse_task.cancel()
+    async def _state_stopwatch(self):
+        if (self._stopwatch.FLAG
+                and self._stopwatch.RUNNING):
+            return True
+        return False
 
+    async def _state_machine(self):
+        if (self._current_state
+                and self._current_state == self._next_state
+                and self._mouse_state):
+            return True
+        return False
 
-# Start stopwatch function
-async def monitor_win():
-    while True:
-        current_state = await get_win_title()  # Catch active window
-        print(current_state)
-        await manage_stopwatch(sw, current_state, current_state)  # Start stopwatch
+    async def _manage_stopwatch(self):
+        mouse_task = asyncio.create_task(self._mouse_pos())
+        self._elapsed_time = self._stopwatch.ELAPSED_TIME.format(0, 0, 1)
+        self._stopwatch.FLAG = True
+        self._mouse_state = True
+        try:
+            while await self._state_stopwatch():
+                if await self._state_machine():
+                    print(f'\r{self._elapsed_time}', end=' ')
+                    self._elapsed_time = await self._stopwatch.increment()
+                    self._next_state = await self._get_title()
+                else:
+                    self._stopwatch.FLAG = False
+                    await self._stopwatch.stop()
+                await asyncio.sleep(1)
+        finally:
+            mouse_task.cancel()
+
+    async def monitor_window(self):
+        while True:
+            self._current_state = await self._get_title()
+            self._next_state = await self._get_title()
+            print(self._current_state)
+            await self._manage_stopwatch()
 
 
 async def main():
-    win_mon = asyncio.create_task(monitor_win())
+    user_activity = UserActivity()
+    monitor_window = asyncio.create_task(user_activity.monitor_window())
     try:
         while True:
-            await win_mon
             await asyncio.sleep(1)
     except KeyboardInterrupt:
+        monitor_window.cancel()
         print('interrupted')
     except Exception as e:
         print(f'EXCEPTION {e}')

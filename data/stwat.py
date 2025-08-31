2f1a54bdb0a5748078c629a6516a6c5f0ed190e7
Taplenij <bodilovskijegor87@gmail.com>
2025-08-31 16:50:17 +0300

    Forced_stop was deleted, grab_current_time was added to grab current time, way of calculation in increment method was corrected

--- data/stwat.py
+++ data/stwat.py
@@ -6,19 +6,22 @@ class StopWatch:
     FLAG = False
     ONE_SEC = timedelta(seconds=1)
     ELAPSED_TIME = 'time_elapsed: {:.0f}h {:>2.0f}m {:>2.0f}s'
-    START_TIME = dict(hours=0, minutes=0, seconds=1)
+    START_TIME = dict(hours=0, minutes=0, seconds=0)
     START_STATE = _CURRENT_STATE = timedelta(**START_TIME)
 
     async def stop(self):
         self._CURRENT_STATE = self.START_STATE
         print('Stop')
 
-    async def forced_stop(self):
-        self.RUNNING = False
-
     async def increment(self):
         self._CURRENT_STATE += self.ONE_SEC
         hours = self._CURRENT_STATE.seconds // 3600
-        minutes = self._CURRENT_STATE.seconds // 60
+        minutes = (self._CURRENT_STATE.seconds // 60) % 60
         seconds = self._CURRENT_STATE.seconds % 60
         return self.ELAPSED_TIME.format(hours, minutes, seconds)
+
+    async def grab_current_time(self):
+        return dict(hour=(self._CURRENT_STATE.seconds // 3600),
+                    minute=((self._CURRENT_STATE.seconds // 60) % 60),
+                    second=(self._CURRENT_STATE.seconds % 60)
+                    )

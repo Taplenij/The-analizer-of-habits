4022afd779b66f351517d3dcff257b4be63c0d6e
Taplenij <bodilovskijegor87@gmail.com>
2025-08-31 17:00:34 +0300

    handlers file was created

---
+++ data/tg_bot/handlers.py
@@ -0,0 +1,5 @@
+from aiogram import Router
+from aiogram.types import Message, CallbackQuery
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.context import FSMContext
+from data.requests import DBC

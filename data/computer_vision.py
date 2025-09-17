8809331eaa1c7d4ddb8b0fa67467029dca5d35c8
Taplenij <bodilovskijegor87@gmail.com>
2025-09-17 21:03:27 +0300

    Created class Computer_vision with async methods

--- data/computer_vision.py
+++ data/computer_vision.py
@@ -1,22 +1,44 @@
 from PIL import ImageGrab
+import asyncio
 import pytesseract
-import time
+import logging
 import re
 
+log = logging.getLogger('computer_vision')
+log.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+
+formatter = logging.Formatter(
+    fmt='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+
+ch.setFormatter(formatter)
+log.addHandler(ch)
 
 class ComputerVision:
     SOC_NET = ['discord', 'tiktok', 'snapchat', 'youtube',
-               'viber', 'facebook', 'vk', 'telegram', 'instagram']
+               'viber', 'facebook', 'vk',
+               'telegram', 'instagram']
     TEXT = None
 
-    def read_text_from_win(self):
-        time.sleep(2)
-        image = ImageGrab.grab(bbox=(200, 40, 1500, 70))
-        image.save('test.jpg')
-        self.TEXT = pytesseract.image_to_string(image)
+    async def read_text_from_win(self):
+        try:
+            await asyncio.sleep(2)
+            image = ImageGrab.grab(bbox=(200, 40, 1500, 70))
+            image.save('test.jpg')
+            self.TEXT = pytesseract.image_to_string(image)
+        except KeyboardInterrupt:
+            log.info('Shutting down')
 
-    def active_win_info(self):
-        pass
-
-c = ComputerVision()
-c.active_win_info()
+    async def active_win_info(self):
+        if not self.TEXT:
+            log.info('Text is empty')
+        else:
+            find_text = re.findall(r'(?:\w+\.)+\w+', self.TEXT)
+            find_text = (' '.join(find_text)).split('.')[1]
+            log.info(find_text)
+            if find_text in self.SOC_NET:
+                log.info(f'Found SOC NET {find_text}')
+            else:
+                log.info(f'Found TEXT {find_text}')

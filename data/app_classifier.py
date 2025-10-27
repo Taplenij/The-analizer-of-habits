0145990086fc3a673b1b9d9ef6ee858fa2c4c27e
Taplenij <bodilovskijegor87@gmail.com>
2025-10-27 19:56:55 +0300

    new

--- data/app_classifier.py
+++ data/app_classifier.py
@@ -1,1 +1,6 @@
 from sentence_transformers import SentenceTransformer
+
+model = SentenceTransformer('paraphrase-multilingual-MiniLM-L12-v2')
+sentence = ['Gucci and Lui Vuitton sucks', 'Nike, puma and adidas are kings']
+embedings = model.encode(sentence)
+print(embedings)

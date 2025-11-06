11438c7155b26dcc8ff2709a2e9c923b56dfc99e
Taplenij <bodilovskijegor87@gmail.com>
2025-11-06 16:15:33 +0300

    New file for learn if have interest

---
+++ data/classification/create_embeddings.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import pyprind
+from sentence_transformers import SentenceTransformer
+from data.classification.data_clf import dict_apps
+
+df = pd.DataFrame()
+pg = pyprind.ProgBar(iterations=304)
+transformer = SentenceTransformer('paraphrase-MiniLM-L12-v2')
+
+for l in dict_apps:
+    for n in dict_apps[l]:
+        embeddings = transformer.encode(n)
+        df = df._append([[l, *embeddings]], ignore_index=True)
+        pg.update()
+
+df.columns = ['category', *[f'values{i}' for i in range(1, 385)]]
+
+df.to_csv('model_app_data.csv', index=False, encoding='utf-8')

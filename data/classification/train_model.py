1d9d8cbc4202f444a7fd447bac5e7009287ff3db
Taplenij <bodilovskijegor87@gmail.com>
2025-11-15 20:43:42 +0300

    Bagging not as good as rfc

--- data/classification/train_model.py
+++ data/classification/train_model.py
@@ -7,6 +7,15 @@ from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
 from sklearn.metrics import accuracy_score
 from sklearn.pipeline import make_pipeline, Pipeline
+from sklearn.tree import DecisionTreeClassifier
+
+df = pd.read_csv('tables/model_app_data_multi-qa-mpnet-base-cos-v1.csv')
+X = df.iloc[:, 1:].values
+y = df.iloc[:, 0].values
+le = LabelEncoder()
+y = le.fit_transform(y)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, stratify=y,
+                                                    shuffle=True, random_state=1)
 
 def get_score_info(clf, X_train, y_train, X_test, y_test):
     cv_score = cross_val_score(clf, X_train, y_train, cv=10, n_jobs=1)
@@ -28,9 +37,8 @@ def get_score_info(clf, X_train, y_train, X_test, y_test):
 
 def check_stats_for_several_models(clf, method):
     cur_path = os.getcwd()
-    target_path = cur_path + 'tables'
-    for table in os.listdir(target_path):
-        df = pd.read_csv(table, encoding='utf-8')
+    for table in os.listdir(os.path.join(cur_path, 'tables')):
+        df = pd.read_csv(os.path.join('tables', table), encoding='utf-8')
         X = df.iloc[:, 1:].values
         y = df.iloc[:, 0].values
         le = LabelEncoder()
@@ -44,30 +52,28 @@ def check_stats_for_several_models(clf, method):
             X_test_lda = pipe.transform(X_test)
             print(f'Score info with {table}')
             get_score_info(clf, X_train_lda, y_train, X_test_lda, y_test)
-        elif method == 'pipeline':
+            print('--' * 50)
+        elif not isinstance(clf, Pipeline) and method == 'pipeline':
             pipe = make_pipeline(StandardScaler(), LDA(n_components=5), clf)
             print(f'Score info with {table}')
             get_score_info(pipe, X_train, y_train, X_test, y_test)
+            print('--' * 50)
         else:
             raise ValueError('Method parameter must be hand or pipeline')
-#
+# RANDOM FOREST
 # rfc = RandomForestClassifier(criterion='gini', max_depth=10,
 #                              max_leaf_nodes=10, min_impurity_decrease=0.0001,
 #                              min_samples_split=5, min_samples_leaf=5,
 #                              n_estimators=50)
-#
-# pipe_lda = make_pipeline(StandardScaler(), LDA(n_components=5))
-# X_train_lda = pipe_lda.fit_transform(X_train, y_train)
-# X_test_lda = pipe_lda.transform(X_test)
-# print('without pipeline:')
-# get_score_info(rfc, X_train_lda, y_train, X_test_lda, y_test)
-#
-# pipe_rfc_lda = make_pipeline(StandardScaler(), LDA(n_components=5),
-#                              RandomForestClassifier(criterion='gini', max_depth=10,
-#                              max_leaf_nodes=10, min_impurity_decrease=0.0001,
-#                              min_samples_split=5, min_samples_leaf=5,
-#                              n_estimators=50))
-# print('With pipeline:')
-# get_score_info(pipe_rfc_lda, X_train, y_train, X_test, y_test)
-# predict_category('Chivalry 2', rfc, trans)
+# check_stats_for_several_models(rfc, 'hands')
+# print('**' * 50)
+# check_stats_for_several_models(rfc, 'pipeline')
+
 #BAGGING
+tree = DecisionTreeClassifier(criterion='entropy', max_depth=10, random_state=1)
+bag = BaggingClassifier(estimator=tree, max_features=1.0, max_samples=1.0,
+                        bootstrap=True, bootstrap_features=False, n_jobs=1)
+check_stats_for_several_models(bag, 'hands')
+print('**' * 50)
+check_stats_for_several_models(bag, 'pipeline')
+

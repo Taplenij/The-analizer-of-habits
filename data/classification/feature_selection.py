1813c52f55425af9269c5cd91a83650c439460c2
Taplenij <bodilovskijegor87@gmail.com>
2025-12-03 21:03:06 +0300

    new

--- data/classification/feature_selection.py
+++ data/classification/feature_selection.py
@@ -1,27 +1,41 @@
 import numpy as np
+import matplotlib.pyplot as plt
 import pandas as pd
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+from data.classification.SequentialBackwardSelection import SBS
 
 df = pd.read_csv('tables/model_app_data_multi-qa-mpnet-base-cos-v1.csv')
 X = df.iloc[:, 1:].values
 y = df.iloc[:, 0].values
 le = LabelEncoder()
 y = le.fit_transform(y)
+scl = StandardScaler()
+X_std = scl.fit_transform(X)
 
 rfc = RandomForestClassifier(criterion='gini', max_depth=10,
                              max_leaf_nodes=10, min_impurity_decrease=0.0001,
                              min_samples_split=5, min_samples_leaf=5,
                              n_estimators=50)
-rfc.fit(X, y)
-importance_feat = np.array([])
-feat_labels = df.columns[1:]
-importances = rfc.feature_importances_
-indices = np.argsort(importances)[::-1]
-for f in range(X.shape[1]):
-    if importances[f] != 0.0:
-        importance_feat = np.append(importance_feat, feat_labels[f])
-print(importance_feat)
-print(importance_feat.shape)
+# rfc.fit(X, y)
+# importance_feat = np.array([])
+# feat_labels = df.columns[1:]
+# importances = rfc.feature_importances_
+# indices = np.argsort(importances)[::-1]
+# for f in range(X.shape[1]):
+#     if importances[f] != 0.0:
+#         importance_feat = np.append(importance_feat, feat_labels[f])
+# print(importance_feat)
+# print(importance_feat.shape)
 # np.save('new_selection', importance_feat)
+
+sbs = SBS(rfc, k_features=1)
+sbs.fit(X_std, y)
+k_feat = [len(k) for k in sbs.subsets_]
+plt.plot(k_feat, sbs.scores_, marker='o')
+plt.xlabel('Feature quantity')
+plt.ylabel('Correctness')
+plt.grid()
+plt.tight_layout()
+plt.show()

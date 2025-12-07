27b4fde1656c2c5b6fa4fc9d805e45ccaaef37e7
Taplenij <bodilovskijegor87@gmail.com>
2025-12-07 20:01:28 +0300

    create data for tf-idf model

---
+++ data/classification/create_text_data.py
@@ -0,0 +1,31 @@
+from data.classification.data_clf import dict_apps, addit_data
+from data.classification.help_functions import preproc
+import requests
+import pandas as pd
+import pyprind
+
+df = pd.DataFrame()
+user_agent = 'Analizer_of_habits/1.0 (https://github.com/Taplenij/The-analizer-of-habits.git)'
+
+headers = {
+    'User-Agent' : user_agent
+}
+pgb = pyprind.ProgBar(iterations=304)
+for l in dict_apps:
+    for name in dict_apps[l]:
+        r = requests.get(f'https://en.wikipedia.org/api/rest_v1/page/summary/{preproc(name)}',
+                         headers=headers)
+        if r.status_code != 200:
+            new_row_df = pd.DataFrame([{'Category': l,
+                                    'AppName': name,
+                                    'DataText': addit_data[name]}])
+        else:
+            text = r.json()
+            new_row_df = pd.DataFrame([{'Category': l,
+                                        'AppName': name,
+                                        'DataText': text.get('extract', '')}])
+        df = pd.concat([df, new_row_df], ignore_index=True)
+        pgb.update()
+df.columns = ['Category', 'AppName', 'DataText']
+df.to_csv('text_data.csv', index=False, encoding='utf-8')
+print('\nEND')

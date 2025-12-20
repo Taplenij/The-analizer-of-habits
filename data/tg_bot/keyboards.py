26b30b52e8f801c0ae2200f5254da5edb06a2952
Taplenij <bodilovskijegor87@gmail.com>
2025-12-20 20:51:23 +0300

    new

--- data/tg_bot/keyboards.py
+++ data/tg_bot/keyboards.py
@@ -2,5 +2,25 @@ from aiogram.types import (InlineKeyboardMarkup, InlineKeyboardButton,
                            ReplyKeyboardMarkup, KeyboardButton)
 
 keyboard = InlineKeyboardMarkup(inline_keyboard=[[
-    InlineKeyboardButton(text='Начать', callback_data='start'),
-    InlineKeyboardButton(text='Настройки', callback_data='settings')]])
+            InlineKeyboardButton(text='Начать', callback_data='start')
+        ],
+        [
+            InlineKeyboardButton(text='Получить результаты', callback_data='info')
+        ]
+])
+
+types = InlineKeyboardMarkup(inline_keyboard=[[
+        InlineKeyboardButton(text='За один день', callback_data='one'),
+        InlineKeyboardButton(text='За неделю', callback_data='weeknd')
+    ],
+    [
+        InlineKeyboardButton(text='Получить все результаты', callback_data='all')
+    ]
+])
+
+weekend = InlineKeyboardMarkup(inline_keyboard=[
+    [
+        InlineKeyboardButton(text='Общий', callback_data='weeknd_genrl'),
+        InlineKeyboardButton(text='Подробный', callback_data='weekend_detld')
+    ]
+])

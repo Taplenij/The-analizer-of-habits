2c1052a2fb4a0bcec75bfa894a4ba69e389b3be6
Taplenij <bodilovskijegor87@gmail.com>
2025-12-20 20:51:13 +0300

    new

--- data/tg_bot/handlers.py
+++ data/tg_bot/handlers.py
@@ -1,7 +1,7 @@
 import asyncio
 import logging
 from aiogram import Router, F
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery
 from aiogram.filters import Command
 from data.tg_bot.requests import DBC
 import keyboards as kb
@@ -29,16 +29,16 @@ async def com_start(message: Message):
     await message.answer('Привет,'
                          ' я - бот, который занимается анализом твоих действий на компьютере.'
                          ' Нажми кнопку "Начать", чтобы я приступил к работе. Также ты можешь'
-                         ' изменить что-либо в настройках', reply_markup=kb.keyboard)
+                         ' изменить что-либо в настройках.', reply_markup=kb.keyboard)
     await req.create_pool()
     await req.record_id(message.from_user.id)
 
 
 @router.callback_query(F.data == 'start')
-async def first_stp(message: Message):
-    await message.answer('Отлично!👏 Теперь я буду за тобой наблюдать😈')
+async def first_stp(callback: CallbackQuery):
+    await callback.answer('Отлично!👏 Теперь я буду за тобой наблюдать😈')
     log.info('Start tracker')
-    user_activity = UserActivity(message.from_user.id)
+    user_activity = UserActivity(callback.from_user.id)
     monitor_window = asyncio.create_task(user_activity.monitor_window())
     try:
         while True:
@@ -48,3 +48,17 @@ async def first_stp(message: Message):
         print('interrupted')
     except Exception as e:
         print(f'EXCEPTION {e}')
+
+
+@router.callback_query(F.data=='info')
+async def second_stp(callback: CallbackQuery):
+    await callback.answer(' ')
+    await callback.message.answer('Выберите тип выводимых результатов:',
+                                  reply_markup=kb.types)
+
+
+@router.callback_query(F.data=='weeknd')
+async def week(callback: CallbackQuery):
+    await callback.answer(' ')
+    await callback.message.answer('Выберите какой вид результатов хотите изучить:',
+                                  reply_markup=kb.weekend)
